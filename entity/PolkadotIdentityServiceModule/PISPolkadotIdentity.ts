3275dd52be73fc6d3e0ae18797eeefd55599c9c4
Will <willzhaoshihao@gmail.com>
2022-01-10 14:38:02 +0800

    Add field last_update_time

--- entity/PolkadotIdentityServiceModule/PISPolkadotIdentity.ts
+++ entity/PolkadotIdentityServiceModule/PISPolkadotIdentity.ts
@@ -3,6 +3,10 @@ import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
 @Index('pis_identity_pkey', ['id'], { unique: true })
 @Entity('pis_identity', { schema: 'public' })
 export class PISPolkadotIdentity {
+  constructor() {
+    this.lastUpdateTime = new Date();
+  }
+
   @Column({type:'text', name: 'id', primary: true})
   id: string;
 
@@ -35,4 +39,7 @@ export class PISPolkadotIdentity {
 
   @Column({type:'jsonb', name: 'raw_identity_in_json', nullable: true })
   rawIdentityInJson: object | null;
+
+  @Column({type:'timestamp', name: 'last_update_time'})
+  lastUpdateTime: Date;
 }
