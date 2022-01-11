f552fe2c9e0c42189356bccaa5fe0739bb3da154
bianyunjian <byj626680108@gmail.com>
2022-01-11 10:17:41 +0800

    update entity field type

--- entity/PolkadotIdentityServiceModule/PISPolkadotIdentity.ts
+++ entity/PolkadotIdentityServiceModule/PISPolkadotIdentity.ts
@@ -7,39 +7,40 @@ export class PISPolkadotIdentity {
     this.lastUpdateTime = new Date();
   }
 
-  @Column({type:'text', name: 'id', primary: true})
+  @Column({ type: 'varchar', length: 255, name: 'id', primary: true })
   id: string;
 
-  @Column({ type:'text', name: 'display', nullable: true})
+  @Column({ type: 'text', name: 'display', nullable: true })
   display: string | null;
 
-  @Column({type:'text', name: 'legal', nullable: true })
+  @Column({ type: 'text', name: 'legal', nullable: true })
   legal: string | null;
 
-  @Column({type:'text', name: 'web', nullable: true })
+  @Column({ type: 'text', name: 'web', nullable: true })
   web: string | null;
 
-  @Column({type:'text', name: 'riot', nullable: true })
+  @Column({ type: 'text', name: 'riot', nullable: true })
   riot: string | null;
 
-  @Column({type:'text', name: 'email', nullable: true })
+  @Column({ type: 'text', name: 'email', nullable: true })
   email: string | null;
 
-  @Column({type:'text', name: 'image', nullable: true })
+  @Column({ type: 'text', name: 'image', nullable: true })
   image: string | null;
 
-  @Column({type:'text', name: 'twitter', nullable: true })
+  @Column({ type: 'text', name: 'twitter', nullable: true })
   twitter: string | null;
 
-  @Column({ type:'text', name: 'account_public_key', nullable: true})
-  accountPublicKey: string;
-
-  @Column({ type:'text', name: 'account_polkadot'})
-  accountPolkadot: string;
-
-  @Column({type:'jsonb', name: 'raw_identity_in_json', nullable: true })
+  @Column({ type: 'jsonb', name: 'raw_identity_in_json', nullable: true })
   rawIdentityInJson: object | null;
 
-  @Column({type:'timestamp', name: 'last_update_time'})
+  @Column({ type: 'varchar', length: 255, name: 'account_public_key', nullable: true })
+  accountPublicKey: string;
+
+  @Column({ type: 'varchar', length: 255, name: 'account_polkadot' })
+  accountPolkadot: string;
+
+
+  @Column({ type: 'timestamp', name: 'last_update_time' })
   lastUpdateTime: Date;
 }
