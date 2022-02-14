17ebf633216d411957dc822aa5ce1d443529b9f0
bianyunjian <626680108@qq.com>
2022-02-14 15:26:19 +0800

    Update AnalyticsInsightDashboard.entity.ts

--- entity/CustomQueryModule/AnalyticsInsightDashboard.entity.ts
+++ entity/CustomQueryModule/AnalyticsInsightDashboard.entity.ts
@@ -28,7 +28,8 @@ export class AnalyticsInsightDashboard {
     created: string = "Web3go Offical Team";
 
     @ApiProperty({
-        description: 'created timestamp, as Date().getTime()'
+        description: 'created timestamp, as Date().getTime()',
+        default: new Date().getTime()
     })
     @Column({
         type: 'bigint',
@@ -47,6 +48,10 @@ export class AnalyticsInsightDashboard {
     @ApiProperty({ default: '', description: 'image snapshot of dashboard, sample as : https://images.xyz/snapshot.png' })
     @Column({ comment: 'image snapshot of dashboard, sample as : https://images.xyz/snapshot.png' })
     snapshot: string = "";
+
+    @ApiProperty({ default: '', description: 'link of dashboard, sample as : https://mb.web3go.xyz/public/dashboard/52bb4d1c-e0dc-4816-9903-7294349f9960' })
+    @Column({ comment: 'link of dashboard, sample as : https://mb.web3go.xyz/public/dashboard/52bb4d1c-e0dc-4816-9903-7294349f9960' })
+    link: string = "";
 
     @ApiProperty({
         description: 'ext property as JSON format',
